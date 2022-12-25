1a1fe3130a739c982db37bf6345a790e3a9588f5
ckenlin <96581920+ckenlin@users.noreply.github.com>
2022-12-25 20:01:41 +0800

    Update mp3.py

--- mp3.py
+++ mp3.py
@@ -38,5 +38,5 @@ option_music=st.selectbox("選擇音樂",type_list[option_musiclist])
 
  
 #audio_file = open(option_year+'/'+option_music+'.mp3', "rb")           
-audio_file = open(音樂+'/'+option_year+'/'+option_music+'.mp3', "rb")
+audio_file = open('音樂'+'/'+option_year+'/'+option_music+'.mp3', "rb")
 st.audio(audio_file.read())
